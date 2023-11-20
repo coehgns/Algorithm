9d8e64eec5183ab751132f9c17d9c855220bc653
coehgns <149929673+coehgns@users.noreply.github.com>
2023-11-20 22:11:43 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    scanf("%d %d %d",&A,&B,&C);
+    A += C/60;
+    B += C%60;
+    
+    if(B >=60)
+    {
+        ++A;
+        B -= 60;
+    }
+    if(A >= 24)
+    {
+        A -= 24;
+    }
+    
+    printf("%d %d\n", A, B);
+    return 0;
+}
