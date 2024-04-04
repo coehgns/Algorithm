679d0866fdf21ffa935570cb504df5ca39bc490b
도훈 <149929673+coehgns@users.noreply.github.com>
2024-04-04 14:48:27 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int arr[5];
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < 5; i++) {
+		sum += arr[i] * arr[i];
+	}
+	printf("%d", sum % 10);
+
+	return 0;
+}
