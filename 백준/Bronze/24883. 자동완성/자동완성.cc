fc47749cc5f6afe700bd6ac4da50975fd7845c3c
도훈 <149929673+coehgns@users.noreply.github.com>
2024-04-17 08:34:18 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    if(str == "n" || str == "N") {
+        cout << "Naver D2";
+    }
+    else {
+        cout << "Naver Whale";
+    }
+    
+	return 0;
+}
