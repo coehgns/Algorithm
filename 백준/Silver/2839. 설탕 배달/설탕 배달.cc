ec46246cf2b7140f4da80da39999f9122aff932d
도훈 <149929673+coehgns@users.noreply.github.com>
2024-04-23 17:17:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, cnt = 0;
+  cin >> n;
+
+  while(n >= 0) {
+
+    if(n % 5 == 0) {
+      cout << cnt + n / 5;
+      return 0;
+    }
+    n -= 3;
+    cnt++;
+
+  }
+
+  cout << -1;
+
+  return 0;
+}
