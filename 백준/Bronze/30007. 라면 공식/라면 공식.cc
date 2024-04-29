9d9774fda34bbe6f2ee7943d581abb839dc34742
도훈 <149929673+coehgns@users.noreply.github.com>
2024-04-29 13:46:45 +0900

    [Bronze V] Title: 라면 공식, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, A, B, X;
+  cin >> n;
+
+  for(int i = 0; i < n; i++) {
+    cin >> A >> B >> X;
+    cout << A * (X - 1) + B << "\n";
+  }
+
+  return 0;
+}
