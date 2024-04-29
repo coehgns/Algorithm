e633ec2b874678ebe562af05f9ef3a78c4a257c6
도훈 <149929673+coehgns@users.noreply.github.com>
2024-04-29 17:26:27 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int b, b2, w, cd, cw, w2;
+
+  cin >> b >> b2 >> w >> cd >> cw >> w2;
+  cout << b * 5 + b2 * 5 + w * 5 + cd * 5 + cw * 5 + w2 * 5;
+
+  return 0;
+}
