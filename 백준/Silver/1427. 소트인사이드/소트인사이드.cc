b7aa24151b9a7c1b4a301c91a17f562ff9f959e3
도훈 <149929673+coehgns@users.noreply.github.com>
2024-05-01 11:30:59 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+  
+  string str;
+  cin >> str;
+  sort(str.rbegin(), str.rend());
+  cout << str;
+
+  return 0;
+}
