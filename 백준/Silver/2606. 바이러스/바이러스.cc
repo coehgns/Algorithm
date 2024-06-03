80b3cea5bae394c9c53628d8cbc5580c3d02f481
도훈 <149929673+coehgns@users.noreply.github.com>
2024-06-03 19:40:20 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
+
+vector<vector<int>> v(101);
+vector<int> visited(101);
+
+int cnt = 0;
+void dfs(int n) {
+  if(visited[n]) return;
+  visited[n] = 1;
+  cnt++;
+  for(int i : v[n]) dfs(i);
+}
 
 int main() {
   int n, m;
   cin >> n >> m;
-
-  vector<vector<int>> v(n+1);
 
   for(int i = 0; i < m; i++) {
     int a, b;
@@ -15,29 +23,11 @@ int main() {
 
     v[a].push_back(b);
     v[b].push_back(a);
-  }  
-
-  queue<int> q;
-  q.push(1);
- 
-  vector<int> visited(n+1);
-  visited[1] = 1;
-
-  int cnt = 0;
-  while(!q.empty()) {
-    int now = q.front();
-    q.pop();
-
-    for(int i : v[now]) {
-      if(!visited[i]) {
-        visited[i] = 1;
-        cnt++;
-        q.push(i);
-      }
-    }
   }
 
-  cout << cnt;
+  dfs(1);
+
+  cout << cnt - 1;
 
   return 0;
 }
