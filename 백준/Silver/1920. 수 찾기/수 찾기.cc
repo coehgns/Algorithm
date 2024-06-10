f96d0f3b13870af57685f7742992d5db0ca7b264
도훈 <149929673+coehgns@users.noreply.github.com>
2024-06-10 18:52:55 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,30 +1,42 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-
     std::ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-    
-	int n, m, input;
-	set<int> s;
 
-	cin >> n;
-	while (n--) {
+    int n, m;
+    cin >> n;
+    vector<int> v1(n, 0);
 
-		cin >> input;
-		s.insert(input);
-	}
+    for(int i = 0; i < n; i++) 
+        cin >> v1[i];
 
-	cin >> m;
-	while (m--) {
+    sort(v1.begin(), v1.end());
 
-		cin >> input;
-		cout << (s.find(input) != s.end()) << "\n";
-	}
+    cin >> m;
+    for(int i = 0; i < m; i ++) {
+        int target;
+        cin >> target;
 
+        int start = 0;
+        int end = n - 1;
 
-	return 0;
+        int check = 0;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(v1[mid] == target) {
+                check = 1;
+                break;
+            }
+            else if(v1[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+        cout << check << "\n";
+    }
+
+    return 0;
 }
