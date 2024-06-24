434298dfe2fa7f8b56cc06f47d5fc0343edc9c5f
도훈 <149929673+coehgns@users.noreply.github.com>
2024-06-24 14:10:47 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string s;
+    while(!cin.eof()) {
+        getline(cin, s);
+        cout << s << "\n";
+    }
+
+    return 0;
+}
