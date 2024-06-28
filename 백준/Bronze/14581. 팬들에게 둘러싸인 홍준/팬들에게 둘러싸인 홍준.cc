f27355a39e951cda334a857f2409424df40b1a9a
도훈 <149929673+coehgns@users.noreply.github.com>
2024-06-28 10:55:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:" << "\n" << ":fan::" << str << "::fan:" << "\n" << ":fan::fan::fan:"; 
+    
+    return 0;
+}
