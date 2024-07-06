4a97d95d3ca7a100d5bffdffa03cd2444fcdef9b
도훈 <149929673+coehgns@users.noreply.github.com>
2024-07-06 23:33:09 +0900

    [Bronze V] Title: Serca, Time: 128 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int n;
+    cin >> n;
+    while(n--) {
+        cout << " @@@   @@@ " << "\n"; 
+        cout << "@   @ @   @" << "\n";
+        cout << "@    @    @" << "\n";
+        cout << "@         @" << "\n"; 
+        cout << " @       @ " << "\n"; 
+        cout << "  @     @  " << "\n"; 
+        cout << "   @   @   " << "\n"; 
+        cout << "    @ @    " << "\n"; 
+        cout << "     @     " << "\n";;     
+    }
+
+    return 0;
+}
