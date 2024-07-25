e5fc64654682518fb8a27f9687ae46a66c44334e
도훈 <149929673+coehgns@users.noreply.github.com>
2024-07-25 23:51:38 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    int cnt = 0;
+    while(getline(cin, str)) {
+        cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
