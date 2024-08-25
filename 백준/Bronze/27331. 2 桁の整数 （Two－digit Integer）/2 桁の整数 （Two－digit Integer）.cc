22d864d1dc09f107a6b4ad01ada388e853c72ed1
도훈 <149929673+coehgns@users.noreply.github.com>
2024-08-25 23:20:44 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    string str = to_string(a) + to_string(b);
+
+    cout << str;
+
+    return 0;
+}
