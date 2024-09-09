8ff5add3629404dffbc6742c3b2a3483e9a20c25
도훈 <149929673+coehgns@users.noreply.github.com>
2024-09-09 20:14:28 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << 11 << "\n" << "A B C D E F G H J L M";
+    } else if (n == 2) {
+        cout << 9 << "\n" << "A C E F G H I L M";
+    } else if (n == 3) {
+        cout << 9 << "\n" << "A C E F G H I L M";
+    } else if (n == 4) {
+        cout << 9 << "\n" << "A B C E F G H L M";
+    } else if (n == 5) {
+        cout << 8 << "\n" << "A C E F G H L M";
+    } else if (n == 6) {
+        cout << 8 << "\n" << "A C E F G H L M";
+    } else if (n == 7) {
+        cout << 8 << "\n" << "A C E F G H L M";
+    } else if (n == 8) {
+        cout << 8 << "\n" << "A C E F G H L M";
+    } else if (n == 9) {
+        cout << 8 << "\n" << "A C E F G H L M";
+    } else {
+        cout << 8 << "\n" << "A B C F G H L M";
+    }
+
+    return 0;
+}
