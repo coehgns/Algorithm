2ab8be43ff0069da367397bcd96a038e269569e5
도훈 <149929673+coehgns@users.noreply.github.com>
2024-09-10 23:17:07 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int b, g;
+
+    while(1) {
+        cin >> b >> g;
+        if(b == 0 && g == 0) break;
+        cout << b + g << '\n';
+    }
+
+    return 0;
+}
