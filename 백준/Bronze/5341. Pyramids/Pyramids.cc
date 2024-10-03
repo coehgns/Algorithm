f05ee518aff7114c586286480ee50211beede7c4
도훈 <149929673+coehgns@users.noreply.github.com>
2024-10-03 23:13:10 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  while(1) {
+    int n; cin >> n;
+
+    if (n == 0) break;
+      
+    cout << n * (n + 1) / 2 << "\n";
+  }
+
+  return 0;
+}
