efc5633b92587db8f43b63a50a045da859a54d99
도훈 <149929673+coehgns@users.noreply.github.com>
2025-08-04 19:44:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.95 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    
+    unordered_map<string, int> map;
+    for(string name : participant) {
+        map[name]++;
+    }
+    
+    for(string name : completion) {
+        map[name]--;
+    }
+    
+    for(auto pair : map) {
+        if(pair.second == 1) {
+            return pair.first;
+        }
+    }
+    
+}
