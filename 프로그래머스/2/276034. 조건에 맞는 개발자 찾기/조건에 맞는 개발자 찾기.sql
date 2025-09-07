f2a4a20605865301cc495c5a614d02c1257908d5
도훈 <149929673+coehgns@users.noreply.github.com>
2025-09-07 09:25:11 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,4 +1,8 @@
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPERS
-where (DEVELOPERS.SKILL_CODE & (256 | 1024)) <> 0
-order by DEVELOPERS.ID asc
+where (DEVELOPERS.SKILL_CODE & (
+       select sum(CODE)
+       from SKILLCODES
+       where SKILLCODES.NAME in ('Python', 'C#')
+    )) <> 0
+order by ID asc
