5dc89fccbc1c7e10d3103b2879f3a2724d4b2c98
도훈 <149929673+coehgns@users.noreply.github.com>
2025-10-06 22:58:21 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 3.73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    
+    for (int n : array) {
+        string str = to_string(n);
+        for(char s : str) {
+            if(s == '7') answer++;
+        }
+    }
+    
+    return answer;
+}
